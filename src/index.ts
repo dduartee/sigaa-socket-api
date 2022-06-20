46eb2a595b72783f7e003261706f21e1fdd489c6
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2022-06-20 00:36:01 +0000

    undo last commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Server } from "socket.io";
 import { Router } from "./Router";
-import packageJSON from "../package.json";
 
 const io = new Server({
     cors: {
@@ -13,6 +12,5 @@ const PORT = parseInt(process.env.PORT) || 5000;
 
 io.listen(PORT)
 console.log("Servidor Iniciado: " + PORT)
-console.log("Versão: " + packageJSON.version)
 
 io.on('connection', async (socket) => await new Router({ socket, io }).index())

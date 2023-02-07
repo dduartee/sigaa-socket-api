be425ace1aa52a90c121ab42c77da2ba1a0bdebd
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2023-02-07 09:13:29 -0300

    cacheService modificado

--- src/services/cacheService.ts
+++ src/services/cacheService.ts
@@ -1,3 +1,36 @@
 
 import NodeCache from "node-cache";
-export const cacheService = new NodeCache({ useClones: false, stdTTL: 7200 });
+import { IBondDTOProps } from "../DTOs/Bond.DTO";
+import { IUserDTOProps } from "../DTOs/User.DTO";
+import merge from "ts-deepmerge";
+
+export interface ICacheStructure {
+	options: {
+		username: string;
+		JSESSIONID: string;
+		// currentRegistration: string;
+		sigaaURL: string;
+	},
+	user: IUserDTOProps;
+	bonds: IBondDTOProps[]
+	updatedAt: string;
+}
+
+class CacheService extends NodeCache {
+	constructor(options: NodeCache.Options) {
+		super(options);
+	}
+	/**
+	 * merge new data into cache
+	 */
+	update<T>(key: string, value: Partial<T>) {
+		const cache = cacheService.get(key) ?? [];
+		const cacheMerged = merge(cache, value);
+		cacheService.set(key, cacheMerged);
+		return cacheMerged;
+	}
+}
+
+const cacheService = new CacheService({ useClones: false, stdTTL: 7200 });
+
+export { cacheService };

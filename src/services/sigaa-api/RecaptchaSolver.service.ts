02b34e1ec5b771c0921f7944c056e5b4f843a2e1
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2023-04-09 17:41:05 -0300

    verificação da variavel

--- src/services/sigaa-api/RecaptchaSolver.service.ts
+++ src/services/sigaa-api/RecaptchaSolver.service.ts
@@ -1,5 +1,7 @@
 const recaptchaSolver = async (siteKey: string, dataAction: string) => {
-	const { data } = await fetch(`${process.env.RECAPTCHA_SOLVER_URL}/solve/${siteKey}/${dataAction}`).then((res) => res.json());
+	if (!process.env.RECAPTCHA_SOLVER_URL) 
+		throw new Error("RECAPTCHA_SOLVER_URL is not defined");
+	const { data } = await fetch(`http://${process.env.RECAPTCHA_SOLVER_URL}:8000/solve/${siteKey}/${dataAction}`).then((res) => res.json());
 	return data;
 };
 
